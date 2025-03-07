5e999f48a01236e650df80401ac28badd0201250
Olivia Laurel <OLaurel113@gmail.com>
2025-03-07 10:29:18 -0600

    typing file/word list

---
+++ calmkeys/src/components/typing.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect, useRef } from "react";
+
+const WORD_LIST = [
+    "tranquil", "serenity", "willow", "shimmer", "breeze", "balance", "peace", "silence", 
+    "glow", "lotus", "drift", "clarity", "mindful", "kaze", "forest", "whisper", 
+    "stillness", "ocean", "harmony", "murmur", "yugen", "float", "gentle", "meadow", 
+    "river", "presence"
+  ];
+
+export default function TypingGame() {
+    
+  );
+}
--- calmkeys/src/pages/home.tsx
+++ calmkeys/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
+import TypingGame from '../components/typing';
 
 const Home = () => {
     return (
@@ -9,6 +10,8 @@ const Home = () => {
                 <section className="container mx-auto">
                     <h1 className="text-2xl text-black flex justify-center cursive">home</h1>
                 </section>
+
+                <TypingGame/>
             </main>
 
             <Footer/>
