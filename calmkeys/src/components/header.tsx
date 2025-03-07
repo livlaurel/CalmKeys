f724d48ddc6daf6abe01b8ff45e87fda353d1a05
Olivia Laurel <OLaurel113@gmail.com>
2025-03-07 09:50:34 -0600

    logo added

--- calmkeys/src/components/header.tsx
+++ calmkeys/src/components/header.tsx
@@ -4,9 +4,11 @@ const Header = () => {
     return (
         <main className="bg-[#fff9f0] top-0 z-50">
             <nav className="flex justify-between items-center p-4">
-                <a href="/" className="text-3xl text-black flex justify-left cursive">CalmKeys</a>
+                <a href="/" className="text-black flex justify-left">
+                    <img src="/imgs/calmkeys.png" alt="Calm Keys Logo" className="w-48 h-20" />
+                </a>
                 <div className="flex">
-                    <a href="/about" className="mr-4 text-2xl text-black flex justify-left cursive">
+                    <a href="/about" className="mr-4 flex">
                         <Info className="w-6 h-6" /> 
                     </a>
                 </div>
