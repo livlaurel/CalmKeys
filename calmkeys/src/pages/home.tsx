8063aa7978cf6791e171b68fd66de8c3c8fc5528
Olivia Laurel <OLaurel113@gmail.com>
2025-03-07 09:02:35 -0600

    settig up the webpage

--- calmkeys/src/pages/home.tsx
+++ calmkeys/src/pages/home.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>CalmKeys</h1>
+        <main className="bg-[#fff9f0] min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl text-black flex justify-left cursive">CalmKeys</h1>
+            </section>
+        </main>
     )
 }
 
