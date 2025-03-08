033a21f4894317eb23dd0ac2030b4640de9cd6a2
Olivia Laurel <OLaurel113@gmail.com>
2025-03-07 18:30:37 -0600

    fixing the scroll

--- calmkeys/src/pages/home.tsx
+++ calmkeys/src/pages/home.tsx
@@ -3,14 +3,13 @@ import Footer from '../components/footer';
 
 const Home = () => {
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col h-screen'>
             <Header/>
-            <main className="bg-[#fff9f0] min-h-screen p-12">
+            <main className="bg-[#fff9f0] flex-grow p-12">
                 <section className="container mx-auto">
                     <h1 className="text-2xl text-black flex justify-center cursive">home</h1>
                 </section>
             </main>
-
             <Footer/>
         </div>
     )
