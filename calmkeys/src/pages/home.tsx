8ba67ff81ab9d10ba6b312a72f74144cece6b73b
Olivia Laurel <OLaurel113@gmail.com>
2025-03-07 19:12:07 -0600

    putting in mode buttons

--- calmkeys/src/pages/home.tsx
+++ calmkeys/src/pages/home.tsx
@@ -1,13 +1,22 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
+import { RiPlantFill } from "react-icons/ri";
+import { FaQuoteLeft } from "react-icons/fa6";
+import { RiLetterSpacing2 } from "react-icons/ri";
 
 const Home = () => {
     return (
         <div className='flex flex-col h-screen'>
             <Header/>
-            <main className="bg-[#fff9f0] flex-grow p-12">
+            <main className="bg-[#fff9f0] flex-grow p-7">
                 <section className="container mx-auto">
-                    <h1 className="text-2xl text-black flex justify-center cursive">home</h1>
+                    <div className="box box-content rounded-sm text-base p-4 h-110 bg-[#a9b8a0]">
+                        <div className=" box box-content flex justify-between rounded-sm items-center p-4 bg-[#fff9f0]"> 
+                            <button className="flex items-center mr-2"> <RiPlantFill /> Zen </button>
+                            <button className="flex items-center mr-2"> <FaQuoteLeft /> Quote</button>
+                            <button className="flex items-center mr-2"> <RiLetterSpacing2 /> Words </button>
+                        </div>
+                    </div>
                 </section>
             </main>
             <Footer/>
