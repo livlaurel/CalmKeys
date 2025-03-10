4b85d8ad3c86664393fefc7d8b0563cc540c75b0
Olivia Laurel <OLaurel113@gmail.com>
2025-03-09 19:07:23 -0500

    final changes for deploy again

--- calmkeys/src/App.tsx
+++ calmkeys/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home.tsx';
 import About from './pages/about.tsx';
 
-function App(){
-  return(
-    <BrowserRouter>
+function App() {
+  return (
+    <HashRouter>
       <Routes>
         <Route path="/CalmKeys" element={<Home />} />
         <Route path="/CalmKeys/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
 
 export default App;
--- calmkeys/src/components/header.tsx
+++ calmkeys/src/components/header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <main className="bg-[#fff9f0] top-0 z-50">
             <nav className="flex justify-between items-center p-4">
-                <a href="/CalmKeys/" className="text-black flex justify-left">
+                <a href="#/CalmKeys/" className="text-black flex justify-left">
                     <img src={logo} alt="Calm Keys Logo" className="w-48 h-20" />
                 </a>
                 <div className="flex">
-                    <a href="/CalmKeys/about" className="mr-4 flex">
+                    <a href="#/CalmKeys/about" className="mr-4 flex">
                         <BsFillInfoCircleFill className="w-6 h-6" /> 
                     </a>
                 </div>
