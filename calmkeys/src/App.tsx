2f4c9884560a315e277c3a354e2f8e5b1a91ad2e
Olivia Laurel <OLaurel113@gmail.com>
2025-03-13 14:55:57 -0500

    hashrouter

--- calmkeys/src/App.tsx
+++ calmkeys/src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home.tsx';
 import About from './pages/about.tsx';
 
 function App(){
   return(
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/CalmKeys" element={<Home />} />
-        <Route path="/CalmKeys/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
