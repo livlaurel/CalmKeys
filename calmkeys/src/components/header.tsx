5dd518e20343b6806e5dbc176bb5b03a48a0468e
Olivia Laurel <OLaurel113@gmail.com>
2025-03-13 15:02:14 -0500

    fix home route

--- calmkeys/src/components/header.tsx
+++ calmkeys/src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <main className="bg-[#fff9f0] top-0 z-50">
             <nav className="flex justify-between items-center p-4">
-                <a href="/" className="text-black flex justify-left">
+                <a href="/CalmKeys" className="text-black flex justify-left">
                     <img src={logo} alt="Calm Keys Logo" className="w-48 h-20" />
                 </a>
                 <div className="flex">
